f3fe4fa46f46e919b9217fff0dcc8a11eb7b8a5a
Boeing737ng <ghlove03187@naver.com>
2021-09-23 11:13:47 +0900

    First Commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by Kihyun Choi on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
